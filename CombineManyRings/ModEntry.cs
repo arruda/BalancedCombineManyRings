c543fe9e5d02bb5eeeeada62f6b3e3d7b07805ec
sliva <sliva@DESKTOP-7M27B1J>
2021-05-29 19:52:55 -0400

    Add function to create a short description override for deeply combined rings.

--- CombineManyRings/ModEntry.cs
+++ CombineManyRings/ModEntry.cs
@@ -7,6 +7,7 @@ using StardewValley;
 using StardewValley.Objects;
 using Harmony;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 
 namespace CombineManyRings
 {
@@ -33,11 +34,80 @@ namespace CombineManyRings
                 prefix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.CanCombine_Prefix))
             );
             harmony.Patch(
+                original: AccessTools.Method(typeof(CombinedRing), "loadDisplayFields"),
+                postfix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.LoadDisplayFields_Postfix))
+            );
+            harmony.Patch(
                 original: AccessTools.Method(typeof(CombinedRing), nameof(CombinedRing.drawInMenu), new Type[] {typeof(SpriteBatch), typeof(Vector2), typeof(float), typeof(float), typeof(float), typeof(StackDrawType), typeof(Color), typeof(bool)}),
                 prefix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.DrawInMenu_Prefix))
             );
         }
-
+        public static int GetCombinedRingTotal(Ring ring)
+        {
+            if (ring is CombinedRing)
+            {
+                int count = 0;
+                foreach (Ring r in (ring as CombinedRing).combinedRings)
+                {
+                    count += GetCombinedRingTotal(r);
+                }
+                return count;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        public static SortedDictionary<string, int> GetCombinedRings(Ring ring)
+        {
+            SortedDictionary<string, int> result = new SortedDictionary<string, int>();
+            Queue<Ring> to_process = new Queue<Ring>();
+            to_process.Enqueue(ring);
+            while (to_process.Count > 0) {
+                Ring cur = to_process.Dequeue();
+                ModMonitor.Log($"Processing {cur.DisplayName}", LogLevel.Trace);
+                if (cur is CombinedRing)
+                {
+                    foreach(Ring r in (cur as CombinedRing).combinedRings)
+                    {
+                        to_process.Enqueue(r);
+                    }
+                }
+                else
+                {
+                    string key = cur.displayName;
+                    if (result.TryGetValue(key, out int val))
+                    {
+                        result.Add(key, val + 1);
+                    }
+                    else
+                    {
+                        result.Add(key, 1);
+                    }
+                }
+            }
+            return result;
+        }
+        public static void LoadDisplayFields_Postfix(CombinedRing __instance)
+        {
+            try
+            {
+                if (GetCombinedRingTotal(__instance) >= 8)
+                {
+                    string description = "Many Rings forged into one:\n\n";
+                    foreach (KeyValuePair<string, int> entry in GetCombinedRings(__instance))
+                    {
+                        description += String.Format("{1}x {0}\n", entry.Key, entry.Value);
+                    }
+                    ModMonitor.Log($"Combined Ring description is {description}", LogLevel.Trace);
+                    __instance.description = description.Trim();
+                }
+            }
+            catch (Exception ex)
+            {
+                ModMonitor.Log($"Failed in {nameof(LoadDisplayFields_Postfix)}:\n{ex}", LogLevel.Error);
+            }
+        }
         public static bool CanCombine_Prefix(Ring __instance, Ring ring, ref bool __result)
         {
             try
