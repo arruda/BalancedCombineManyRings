b05853430feea0a869a8743547ee402f6c270563
sliva <sliva@DESKTOP-7M27B1J>
2021-05-29 17:56:46 -0400

    Quick cleanup of Stardew,Objects prefix to a using directive.

--- CombineManyRings/ModEntry.cs
+++ CombineManyRings/ModEntry.cs
@@ -4,6 +4,7 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Objects;
 using Harmony;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -28,23 +29,23 @@ namespace CombineManyRings
             var harmony = HarmonyInstance.Create("Stari.CombineManyRings");
 
             harmony.Patch(
-                original: AccessTools.Method(typeof(StardewValley.Objects.Ring), nameof(StardewValley.Objects.Ring.CanCombine)),
+                original: AccessTools.Method(typeof(Ring), nameof(Ring.CanCombine)),
                 prefix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.CanCombine_Prefix))
             );
             harmony.Patch(
-                original: AccessTools.Method(typeof(StardewValley.Objects.CombinedRing), nameof(StardewValley.Objects.CombinedRing.drawInMenu), new Type[] {typeof(SpriteBatch), typeof(Vector2), typeof(float), typeof(float), typeof(float), typeof(StackDrawType), typeof(Color), typeof(bool)}),
+                original: AccessTools.Method(typeof(CombinedRing), nameof(CombinedRing.drawInMenu), new Type[] {typeof(SpriteBatch), typeof(Vector2), typeof(float), typeof(float), typeof(float), typeof(StackDrawType), typeof(Color), typeof(bool)}),
                 prefix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.DrawInMenu_Prefix))
             );
         }
 
-        public static bool CanCombine_Prefix(StardewValley.Objects.Ring __instance, StardewValley.Objects.Ring ring, ref bool __result)
+        public static bool CanCombine_Prefix(Ring __instance, Ring ring, ref bool __result)
         {
             try
             {
                 __result = true;
-                if (ring is StardewValley.Objects.CombinedRing)
+                if (ring is CombinedRing)
                 {
-                    foreach (StardewValley.Objects.Ring combinedRing in (ring as StardewValley.Objects.CombinedRing).combinedRings)
+                    foreach (Ring combinedRing in (ring as CombinedRing).combinedRings)
                     {
                         if (!__instance.CanCombine(combinedRing))
                         {
@@ -53,9 +54,9 @@ namespace CombineManyRings
                         }
                     }
                 }
-                else if (__instance is StardewValley.Objects.CombinedRing)
+                else if (__instance is CombinedRing)
                 {
-                    foreach (StardewValley.Objects.Ring combinedRing in (__instance as StardewValley.Objects.CombinedRing).combinedRings)
+                    foreach (Ring combinedRing in (__instance as CombinedRing).combinedRings)
                     {
                         if (!combinedRing.CanCombine(ring))
                         {
@@ -76,19 +77,19 @@ namespace CombineManyRings
                 return true; // run original logic
             }
         }
-        public static bool DrawInMenu_Prefix(StardewValley.Objects.CombinedRing __instance, SpriteBatch spriteBatch, Vector2 location, float scaleSize, float transparency, float layerDepth, StackDrawType drawStackNumber, Color color, bool drawShadow)
+        public static bool DrawInMenu_Prefix(CombinedRing __instance, SpriteBatch spriteBatch, Vector2 location, float scaleSize, float transparency, float layerDepth, StackDrawType drawStackNumber, Color color, bool drawShadow)
         {
             try
             {
                 if (__instance.combinedRings.Count >= 2)
                 {
                     // Always use base rings as the sprites to draw. The first pair that are combined on the left hand side get used as the sprite.
-                    if (__instance.combinedRings[0] is StardewValley.Objects.CombinedRing)
+                    if (__instance.combinedRings[0] is CombinedRing)
                     {
                         __instance.combinedRings[0].drawInMenu(spriteBatch, location, scaleSize, transparency, layerDepth, drawStackNumber, color, drawShadow);
                         return false; // don't run original logic
                     }
-                    else if (__instance.combinedRings[1] is StardewValley.Objects.CombinedRing)
+                    else if (__instance.combinedRings[1] is CombinedRing)
                     {
                         __instance.combinedRings[1].drawInMenu(spriteBatch, location, scaleSize, transparency, layerDepth, drawStackNumber, color, drawShadow);
                         return false; // don't run original logic
